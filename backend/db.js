be75bba1896290f2b61f0f7a0c5cd10707a25930
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-09-01 07:50:27 -0700

    the databse ae made well

--- backend/db.js
+++ backend/db.js
@@ -8,52 +8,28 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_NAME:", process.env.DB_NAME);
 
 const pool = mysql.createPool({
-
     host: process.env.DB_HOST || "localhost",
-
     user: process.env.DB_USER || "root",
-
     password: process.env.DB_PASSWORD || "",
-
     database: process.env.DB_NAME || "bmw_travel",
-
-    port: process.env.DB_PORT || 3306,
+    port: Number(process.env.DB_PORT) || 3306,
 
     waitForConnections: true,
-
     connectionLimit: 10,
-
     queueLimit: 0
-
 });
-
-
-/* =========================
-   TEST DATABASE CONNECTION
-========================= */
 
 pool.getConnection((error, connection) => {
 
     if (error) {
-
-        console.error(
-            "❌ MySQL Connection Failed:"
-        );
-
+        console.error("❌ MySQL Connection Failed:");
         console.error(error.message);
-
         return;
     }
 
-
-    console.log(
-        "✅ MySQL Connected Successfully"
-    );
-
+    console.log("✅ MySQL Connected Successfully");
 
     connection.release();
-
 });
-
 
 module.exports = pool;
